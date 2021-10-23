d59d4ca08bd1aae6e1a0ade8bf2fb68e4c63f4ad
Saidileep Yadlapalli <80979439+Dileep1714@users.noreply.github.com>
2021-10-23 11:21:24 +0530

    Update autoscroller.sh

--- autoscroller.sh
+++ autoscroller.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-xinput set-prop 23 "libinput Scroll Method Enabled" 0, 0, 1
-xinput set-prop 23 "libinput Button Scrolling Button" 2
+xinput set-prop id "libinput Scroll Method Enabled" 0, 0, 1
+xinput set-prop id "libinput Button Scrolling Button" 2
